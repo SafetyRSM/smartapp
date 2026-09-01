da8ef8e83d34fbbfbe363c8fe061433b24e282d9
SafetyRSM <156270311+SafetyRSM@users.noreply.github.com>
2026-09-01 09:23:58 +0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,8 @@
+const CACHE_NAME = 'smartapp-v1';
+self.addEventListener('install', e => self.skipWaiting());
+self.addEventListener('activate', e => self.clients.claim());
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
